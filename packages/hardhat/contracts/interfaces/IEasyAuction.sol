463b2223650f7f4fea6d23be10082ad721f585bb
Anton Cheng <antonassocareer@gmail.com>
2021-06-08 14:10:55 +0800

    refactor: refactor ShortOTokenAction with ShortOTokenUtils

---
+++ packages/hardhat/contracts/interfaces/IEasyAuction.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.2;
+pragma experimental ABIEncoderV2;
+
+interface IEasyAuction {
+    
+    struct AuctionData {
+        address auctioningToken;
+        address biddingToken;
+        uint256 orderCancellationEndDate;
+        uint256 auctionEndDate;
+        bytes32 initialAuctionOrder;
+        uint256 minimumBiddingAmountPerOrder;
+        uint256 interimSumBidAmount;
+        bytes32 interimOrder;
+        bytes32 clearingPriceOrder;
+        uint96 volumeClearingPriceOrder;
+        bool minFundingThresholdNotReached;
+        bool isAtomicClosureAllowed;
+        uint256 feeNumerator;
+        uint256 minFundingThreshold;
+    }
+
+    // @dev: function to intiate a new auction
+    // Warning: In case the auction is expected to raise more than
+    // 2^96 units of the biddingToken, don't start the auction, as
+    // it will not be settlable. This corresponds to about 79
+    // billion DAI.
+    //
+    // Prices between biddingToken and auctioningToken are expressed by a
+    // fraction whose components are stored as uint96.
+    function initiateAuction(
+        address _auctioningToken,
+        address _biddingToken,
+        uint256 orderCancellationEndDate,
+        uint256 auctionEndDate,
+        uint96 _auctionedSellAmount,
+        uint96 _minBuyAmount,
+        uint256 minimumBiddingAmountPerOrder,
+        uint256 minFundingThreshold,
+        bool isAtomicClosureAllowed,
+        address accessManagerContract,
+        bytes memory accessManagerContractData
+    ) external returns (uint256);
+
+    function placeSellOrders(
+        uint256 auctionId,
+        uint96[] memory _minBuyAmounts,
+        uint96[] memory _sellAmounts,
+        bytes32[] memory _prevSellOrders,
+        bytes calldata allowListCallData
+    ) external returns (uint64 userId);
+
+    function placeSellOrdersOnBehalf(
+        uint256 auctionId,
+        uint96[] memory _minBuyAmounts,
+        uint96[] memory _sellAmounts,
+        bytes32[] memory _prevSellOrders,
+        bytes calldata allowListCallData,
+        address orderSubmitter
+    ) external returns (uint64 userId);
+
+    function cancelSellOrders(uint256 auctionId, bytes32[] memory _sellOrders) external;
+
+    function precalculateSellAmountSum(
+        uint256 auctionId,
+        uint256 iterationSteps
+    ) external;
+
+    function settleAuctionAtomically(
+        uint256 auctionId,
+        uint96[] memory _minBuyAmount,
+        uint96[] memory _sellAmount,
+        bytes32[] memory _prevSellOrder,
+        bytes calldata allowListCallData
+    ) external ;
+
+    // @dev function settling the auction and calculating the price
+    function settleAuction(uint256 auctionId) external returns (bytes32 clearingOrder);
+   
+    function claimFromParticipantOrder(
+        uint256 auctionId,
+        bytes32[] memory orders
+    ) external returns ( uint256 sumAuctioningTokenAmount, uint256 sumBiddingTokenAmount);
+
+    function registerUser(address user) external returns (uint64 userId);
+
+    function getUserId(address user) external returns (uint64 userId);
+
+    function getSecondsRemainingInBatch(uint256 auctionId) external view returns (uint256);
+
+    function containsOrder(uint256 auctionId, bytes32 order) external view returns (bool);
+}
---
+++ packages/hardhat/contracts/utils/ShortOTokenUtils.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.2;
+pragma experimental ABIEncoderV2;
+
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import { AuctionBase } from '../utils/AuctionBase.sol';
+import { RollOverBase } from '../utils/RollOverBase.sol';
+
+import { SwapTypes } from '../libraries/SwapTypes.sol';
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import { SafeMath } from '@openzeppelin/contracts/math/SafeMath.sol';
+
+import { IController } from '../interfaces/IController.sol';
+import { IAction } from '../interfaces/IAction.sol';
+import { IOracle } from '../interfaces/IOracle.sol';
+import { IOToken } from '../interfaces/IOToken.sol';
+
+contract ShortOTokenUtils {
+  IController controller;
+  
+  function _initShort (address _controller) internal {
+     controller = IController(_controller);
+  }
+
+  /**
+   * @dev open vault with vaultId 1. this should only be performed once when contract is initiated
+   */
+  function _openGammaVault(uint256 _vaultType) internal {
+
+    bytes memory data;
+    if (_vaultType != 0) {
+      data = abi.encode(_vaultType);
+    }
+
+    // this action will always use vault id 0
+    IController.ActionArgs[] memory actions = new IController.ActionArgs[](1);
+
+    actions[0] = IController.ActionArgs(
+        IController.ActionType.OpenVault,
+        address(this), // owner
+        address(0), // second address
+        address(0), // asset, otoken
+        1, // vaultId
+        0, // amount
+        0, // index
+        data // data
+    );
+
+    controller.operate(actions);
+  }
+
+  /**
+   * @dev mint otoken in vault 0
+   */
+  function _mintOTokens(address _collateral, uint256 _collateralAmount, address _otoken, uint256 _otokenAmount) internal {
+    // this action will always use vault id 0
+    IController.ActionArgs[] memory actions = new IController.ActionArgs[](2);
+
+    actions[0] = IController.ActionArgs(
+        IController.ActionType.DepositCollateral,
+        address(this), // vault owner
+        address(this), // deposit from this address
+        _collateral, // collateral asset
+        1, // vaultId
+        _collateralAmount, // amount
+        0, // index
+        "" // data
+    );
+
+    actions[1] = IController.ActionArgs(
+        IController.ActionType.MintShortOption,
+        address(this), // vault owner
+        address(this), // mint to this address
+        _otoken, // otoken
+        1, // vaultId
+        _otokenAmount, // amount
+        0, // index
+        "" // data
+    );
+
+    controller.operate(actions);
+  }
+
+  /**
+   * @dev settle vault 0 and withdraw all locked collateral
+   */
+  function _settleGammaVault() internal {
+
+    IController.ActionArgs[] memory actions = new IController.ActionArgs[](1);
+    // this action will always use vault id 1
+    actions[0] = IController.ActionArgs(
+        IController.ActionType.SettleVault,
+        address(this), // owner
+        address(this), // recipient
+        address(0), // asset
+        1, // vaultId
+        0, // amount
+        0, // index
+        "" // data
+    );
+
+    controller.operate(actions);
+  }
+
+  
+  
+}
