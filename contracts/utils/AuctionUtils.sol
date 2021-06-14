306dcfec6370b187e83d7b5896fcd9ada677d92c
Anton Cheng <antonassocareer@gmail.com>
2021-06-14 14:38:23 +0800

    build: add placeSellOrders for Gnosis auction

--- contracts/utils/AuctionUtils.sol
+++ contracts/utils/AuctionUtils.sol
@@ -3,16 +3,45 @@ pragma solidity >=0.7.2;
 pragma experimental ABIEncoderV2;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IEasyAuction} from "../interfaces/IEasyAuction.sol";
 import {SwapTypes} from "../libraries/SwapTypes.sol";
 
 contract AuctionUtils {
+  using SafeERC20 for IERC20;
+  using SafeMath for uint256;
+
   IEasyAuction public auction;
 
+  /**
+   * initialize the auction utils by putting in the gonsis safe easy auction address
+   */
   function _initAuction(address _easyAuction) internal {
     if (_easyAuction == address(0)) return;
     auction = IEasyAuction(_easyAuction);
     auction.registerUser(address(this));
+  }
+
+  /**
+   * participate in an auction.
+   */
+  function bidInAuction(
+    address biddingAsset,
+    uint256 auctionId,
+    uint96[] memory _minBuyAmounts,
+    uint96[] memory _sellAmounts,
+    bytes32[] memory _prevSellOrders,
+    bytes calldata allowListCallData
+  ) internal {
+    // approve auction to pull tokens from this contract
+    uint256 sumSellAmount;
+    for (uint256 i = 0; i < _sellAmounts.length; i++) {
+      sumSellAmount = sumSellAmount.add(_sellAmounts[i]);
+    }
+    IERC20(biddingAsset).safeApprove(address(auction), sumSellAmount);
+
+    auction.placeSellOrders(auctionId, _minBuyAmounts, _sellAmounts, _prevSellOrders, allowListCallData);
   }
 
   function _startAuction(
@@ -28,7 +57,7 @@ contract AuctionUtils {
   ) internal {
     address accessManager = address(0);
 
-    IERC20(_auctioningToken).approve(address(auction), _auctionedSellAmount);
+    IERC20(_auctioningToken).safeApprove(address(auction), _auctionedSellAmount);
 
     auction.initiateAuction(
       _auctioningToken,
