99a6bea64240f8ae73ca86fc65501ef7565e058c
Anton Cheng <antonassocareer@gmail.com>
2021-06-23 16:34:45 +0800

    misc: improve scripts for auto trade for example MyAction contract

--- scripts/create-airswap-order-and-trade.ts
+++ scripts/create-airswap-order-and-trade.ts
@@ -1,34 +1,36 @@
-import { run, ethers } from "hardhat";
-
+import { ethers } from "hardhat";
+import { BigNumber } from 'ethers'
 const { createOrder, signOrder } = require('@airswap/utils');
 
 async function main() {
 
-  const otokenToBuy = '0xbceb20506a60a59a45109e12d245ac7e2daf2f60' // sender token
-  const weth = '0xd0a1e359811322d97991e03f863a0c30c2cf029c' // signer token
-  
+  // edit these
+  const otokenToBuy = '' // sender token
+  const actionAddress = '' // my action 
+
+  // kovan weth and airswap address
+  const wethAddress = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
   const swap = '0x79fb4604f2D7bD558Cda0DFADb7d61D98b28CA9f'
-  const action = '0xcA50033F6c3e286D9891f6658298f6EbfD9A8D43'
   
-  const [, signer] = await ethers.getSigners();
+  const [owner, signer] = await ethers.getSigners();
 
   // amount of otoken to buy
-  const senderAmount = (0.9 * 1e8).toString()
-  const collateralAmount = (0.9 * 1e18).toString()
+  const senderAmount = (0.1 * 1e8).toString()
+  const collateralAmount = (0.1 * 1e18).toString()
 
   // amount of weth signer is paying
-  const signerAmount = (0.1 * 1e18).toString()
+  const signerAmount = (0.05 * 1e18).toString()
 
   // use the second address derived from the mnemonic
   
   const order = createOrder({
     signer: {
       wallet: signer.address,
-      token: weth,
+      token: wethAddress,
       amount: signerAmount,
     },
     sender: {
-      wallet: action,
+      wallet: actionAddress,
       token: otokenToBuy,
       amount: senderAmount,
     },
@@ -37,10 +39,18 @@ async function main() {
 
   const signedOrder = await signOrder(order, signer, swap);
 
-  // Fill the order!
-  const MyAction = await ethers.getContractFactory('MyAction');
-  const myAction = MyAction.attach(action)
-  await myAction.mintAndSellOToken(collateralAmount, senderAmount, signedOrder)
+  // check signer weth allowance
+  const weth = await ethers.getContractAt('IWETH', wethAddress);
+
+  const allowance: BigNumber = await weth.allowance(signer.address, swap)
+  if (allowance.lt(signerAmount)) {
+    await weth.connect(signer).approve(swap, signerAmount);
+  }
+
+  // Owner the order!
+  const myAction = await ethers.getContractAt('MyAction', actionAddress);
+  const tx = await myAction.connect(owner).mintAndTradeAirSwapOTC(collateralAmount, senderAmount, signedOrder)
+  console.log(`🍜 OTC order executed done. tx: ${tx.hash}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
